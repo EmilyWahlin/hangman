ec1fae96b6877b00bebb99b1256798ac162e8c99
Emily Wahlin <emilysaletti@gmail.com>
2024-07-14 21:04:59 +0200

    now able to generate random word in console log

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react"
+import words from "./wordList.json"
 
 function App() {
-	const [wordToGuess, setWordGuess] = useState("Test")
+	const [wordToGuess, setWordToGuess] = useState(() => {
+		// in order to generate random word
+		return words[Math.floor(Math.random() * words.length)]
+	})
+	// tracking guessed letters
+	const [guessedLetters, setGuessedLetters] =useState<string[]>([])
 
+	console.log(wordToGuess);
 	return <h1>hi</h1>
 
 }
